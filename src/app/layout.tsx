7516cead86b013b237cd11fa34d28202be8d6933
teninvolves <aerosfenix@gmail.com>
2026-02-09 01:27:58 -0300

    Apk criado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "S.Corp SoftWares Devlopment",
+  title: "App Route-Time",
   description: "RouteTime Tracking",
 };
 
