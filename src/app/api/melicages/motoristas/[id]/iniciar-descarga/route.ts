bf6efeb2d517c5905f4ca9e4d2f3e0824be5381c
teninvolves <aerosfenix@gmail.com>
2026-02-20 04:09:27 -0300

    Erro na importação do arquivo de rotas

--- src/app/api/melicages/motoristas/[id]/iniciar-descarga/route.ts
+++ src/app/api/melicages/motoristas/[id]/iniciar-descarga/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getDatabase } from '@/lib/mongodb';
+import { getDatabase } from '@/app/lib/mongodb';
 import { ObjectId } from 'mongodb';
 
 export async function PUT(
